35938c5fe8d476e0fd101a3b2c3d31802945c471
Vladislav Petrov <electronicchest@gmail.com>
2019-08-12 12:03:15 +0300

    Initial commit

---
+++ TwoInOneTests/TwoInOneTests.swift
@@ -0,0 +1,34 @@
+//
+//  TwoInOneTests.swift
+//  TwoInOneTests
+//
+//  Created by Vladislav Petrov on 12/08/2019.
+//  Copyright © 2019 Vladislav Petrov. All rights reserved.
+//
+
+import XCTest
+@testable import TwoInOne
+
+class TwoInOneTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
